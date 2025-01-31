34cb83446e5355c2bc507c17d2497e356578f72e
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-31 16:29:12 +0100

    fix bug user model no add export default

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,11 @@
 import * as mongoose from "mongoose";
-import {model } from "mongoose";
+import { model } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    created_at: { type: Date, require: true, default: new Date() },
-    updated_at: { type: Date, require: true, default: new Date() },
-})
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  created_at: { type: Date, require: true, default: new Date() },
+  updated_at: { type: Date, require: true, default: new Date() },
+});
+
+export default model("users", userSchema);
