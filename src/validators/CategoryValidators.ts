5834cf5ce494c5ef53f4377036eb1d3193f91fdd
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-02-04 15:57:36 +0100

    fix typo in category validator

--- src/validators/CategoryValidators.ts
+++ src/validators/CategoryValidators.ts
@@ -3,7 +3,7 @@ import { body, query } from "express-validator";
 export class CategoryValidators {
   static addCategory() {
     return [
-      body("title", "Name is requied").isString(),
+      body("title", "Title is requied").isString(),
       body("image", "Image is required").custom((image, { req }) => {
         if (req.file) {
           return true;
